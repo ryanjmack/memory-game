0962084bd584e6dd7a404206222a80daf420d514
root <ryanjmack21@gmail.com>
2018-09-16 11:14:46 -0400

    Move state management functions from Board component to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,15 +17,105 @@ class App extends React.Component {
         [13, 13, 14, 14, 15, 15],
         [16, 16, 17, 17, 18, 18]
       ]),
+      lastClicked: null,
       pairsFound: 0
     })
 
+    this.handleClick = this.handleClick.bind(this);
     this.resetApp = this.resetApp.bind(this);
   }
 
-  // function that shuffles the board and resets the state of the game
-  resetApp(e) {
 
+  // function that shuffles the board and resets the state of the game
+  resetApp(e = null) {
+
+    // if the event is due to the reset button being hit rather than the game being won
+    if (e) {
+      e.preventDefault();
+    }
+
+    // set all the cards to a hidden state
+    [...document.querySelectorAll('.board__card')].forEach(card => {
+      card.classList.remove('board__card--found');
+      card.classList.remove('board__card--active');
+      card.classList.add('board__card--hidden');
+      card.dataset.found = "false";
+    });
+
+    setTimeout(() => {
+      // shuffle the board
+      this.setState({
+        board: shuffle(this.state.board),
+        lastClicked: null,
+        pairsFound: 0
+      });
+    }, 200);
+
+  }
+
+
+  /***********************************************************************
+  * Handles click event on any of the cards in the Game
+  *
+  * If the card is the first card clicked, keep it active
+  * Otherwise we need to compare it to previously clicked element
+  * -- If it matches, make them visible with a class of found
+  * -- Otherwise revert the state of each card back to hidden
+  ***********************************************************************/
+  handleClick(e) {
+    const currClick = e.target;
+
+    // error checks
+    if (this.state.lastClicked === currClick || e.target.dataset.found === "true") {
+      return;
+    }
+
+    // update the current card to an active state
+    currClick.classList.remove('board__card--hidden');
+    currClick.classList.add('board__card--active');
+
+    // give a little feedback delay
+    setTimeout(() => {
+
+      // this is a comparison click
+      if (this.state.lastClicked) {
+        const prevClick = this.state.lastClicked;
+
+        // the user made a match! set cards to a 'found' state
+        if (prevClick.textContent === currClick.textContent) {
+          currClick.classList.remove('board__card--active');
+          currClick.classList.add('board__card--found');
+
+          prevClick.classList.remove('board__card--active');
+          prevClick.classList.add('board__card--found');
+
+          // used in the error check at the top of this method
+          prevClick.dataset.found = "true";
+          currClick.dataset.found = "true";
+        }
+
+        // wrong guess! revert the cards back to 'hidden' state
+        else {
+          currClick.classList.remove('board__card--active');
+          currClick.classList.add('board__card--hidden');
+
+          prevClick.classList.remove('board__card--active');
+          prevClick.classList.add('board__card--hidden');
+        }
+
+        // next click will be the first click
+        this.setState({
+          lastClicked: null
+        });
+      }
+
+      // not a comparison click, save click reference for next comparison click
+      else {
+        this.setState({
+          lastClicked: currClick
+        });
+      }
+    }, 200);
   }
 
 
@@ -33,7 +123,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>Memory Game</h1>
-        <Board board={this.state.board} />
+        <Board board={this.state.board} handleClick={this.handleClick} />
       </div>
     )
   }
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -13,76 +13,9 @@ class Board extends React.Component {
     // Numbers will be the differentiators for the game
     this.state = {
       board: this.props.board,
-      lastClicked: null
     }
 
     this.createCards = this.createCards.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-
-  /***********************************************************************
-  * Handles click event on any of the cards in the Game
-  *
-  * If the card is the first card clicked, keep it active
-  * Otherwise we need to compare it to previously clicked element
-  * -- If it matches, make them visible with a class of found
-  * -- Otherwise revert the state of each card back to hidden
-  ***********************************************************************/
-  handleClick(e) {
-    const currClick = e.target;
-
-    // error checks
-    if (this.state.lastClicked === currClick || e.target.dataset.found === "true") {
-      return;
-    }
-
-    // update the current card to an active state
-    currClick.classList.remove('board__card--hidden');
-    currClick.classList.add('board__card--active');
-
-    // give a little feedback delay
-    setTimeout(() => {
-
-      // this is a comparison click
-      if (this.state.lastClicked) {
-        const prevClick = this.state.lastClicked;
-
-        // the user made a match! set cards to a 'found' state
-        if (prevClick.textContent === currClick.textContent) {
-          currClick.classList.remove('board__card--active');
-          currClick.classList.add('board__card--found');
-
-          prevClick.classList.remove('board__card--active');
-          prevClick.classList.add('board__card--found');
-
-          // used in the error check at the top of this method
-          prevClick.dataset.found = "true";
-          currClick.dataset.found = "true";
-        }
-
-        // wrong guess! revert the cards back to 'hidden' state
-        else {
-          currClick.classList.remove('board__card--active');
-          currClick.classList.add('board__card--hidden');
-
-          prevClick.classList.remove('board__card--active');
-          prevClick.classList.add('board__card--hidden');
-        }
-
-        // next click will be the first click
-        this.setState({
-          lastClicked: null
-        });
-      }
-
-      // not a comparison click, save click reference for next comparison click
-      else {
-        this.setState({
-          lastClicked: currClick
-        });
-      }
-    }, 200);
   }
 
 
@@ -96,7 +29,7 @@ class Board extends React.Component {
       return (
         // in regards to the arbitrary key values -  // https://stackoverflow.com/a/43892905/6894170
         <div key={i + 100} className='board__row'>
-          {row.map((num, j) => <div key={i + j} className='board__card board__card--hidden' onClick={this.handleClick}>{num}</div>)}
+          {row.map((num, j) => <div key={i + j} className='board__card board__card--hidden' onClick={this.props.handleClick}>{num}</div>)}
         </div>
       )
     });
